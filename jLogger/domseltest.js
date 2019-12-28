be32286fa56cc94d77fa6b2d0c93fd3810e81210
Pramod Jain <pramod.aug17@gmail.com>
2019-12-29 00:51:42 +0530

    Moving to another repo

--- jLogger/domseltest.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(e) {
-    console.log("Test 1");
-    console.log(domselector("<div>"));
-    console.log(domselector("div"));
-    console.log(domselector("div#logBox"));
-    console.log(domselector(".logBox"));
-};
